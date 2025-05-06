17970768d7ac5d2c567a9139698ee011f8a5f76d
EmilianoAlvarado17 <emiliano.alvarado@gebesa.com>
2025-05-06 10:37:56 -0600

    Funcionalidad de Mic
    Solamente en Android, desconozco iOS

--- lib/src/screens/agent/agent_screen.dart
+++ lib/src/screens/agent/agent_screen.dart
@@ -1,84 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
+import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class AgentScreen extends StatefulWidget {
-  const AgentScreen({Key? key}) : super(key: key);
-
+  const AgentScreen({super.key});
   @override
   State<AgentScreen> createState() => _AgentScreenState();
 }
 
 class _AgentScreenState extends State<AgentScreen> {
-  late final WebViewController controller;
+  WebViewController? _controller; // ① ahora es nullable
   bool isLoading = true;
-
-  // Ejecutar JavaScript para solicitar permisos de audio cuando la pu00e1gina termine de cargar
-  Future<void> _requestAudioPermission() async {
-    await Future.delayed(const Duration(
-        seconds: 2)); // Esperar a que la pu00e1gina cargue completamente
-    controller.runJavaScript('''
-      navigator.mediaDevices.getUserMedia({ audio: true })
-        .then(function(stream) {
-          console.log('Micru00f3fono habilitado');
-        })
-        .catch(function(err) {
-          console.error('Error al acceder al micru00f3fono:', err);
-        });
-    ''');
-  }
 
   @override
   void initState() {
     super.initState();
-    // Inicializar el controlador de WebView con configuración para permitir acceso a medios
-    controller = WebViewController()
-      // Permitir JavaScript sin restricciones (necesario para acceso a medios)
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      // Fondo transparente
-      ..setBackgroundColor(const Color(0x00000000))
-      // Configurar User Agent para mejor compatibilidad
-      ..setUserAgent(
-          'Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36')
-      // Añadir canal JavaScript para comunicación
-      ..addJavaScriptChannel(
-        'Toaster',
-        onMessageReceived: (JavaScriptMessage message) {
-          print('WebView message: ${message.message}');
-        },
-      )
-      ..setNavigationDelegate(
-        NavigationDelegate(
-          onProgress: (int progress) {
-            // Actualizar indicador de carga
-            if (progress < 100) {
-              setState(() {
-                isLoading = true;
-              });
-            } else {
-              setState(() {
-                isLoading = false;
-              });
-            }
-          },
-          onPageStarted: (String url) {},
-          onPageFinished: (String url) {
-            setState(() {
-              isLoading = false;
-            });
-          },
-          onWebResourceError: (WebResourceError error) {},
-        ),
-      )
-      ..loadRequest(Uri.parse('https://thunderous-dango-83ccc5.netlify.app/'));
+    _initWebView();
   }
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    // Solicitar permisos de audio despuu00e9s de que el widget estu00e9 completamente inicializado
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _requestAudioPermission();
-    });
+  Future<void> _initWebView() async {
+    await [Permission.microphone].request();
+
+    final ctrl = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      // …todas tus demás llamadas…
+      ..loadRequest(Uri.parse('https://thunderous-dango-83ccc5.netlify.app/'));
+
+    // callbacks específicos de plataforma …
+    if (ctrl.platform is AndroidWebViewController) {
+      final android = ctrl.platform as AndroidWebViewController;
+      android
+        ..setMediaPlaybackRequiresUserGesture(false)
+        ..setOnPlatformPermissionRequest((request) async {
+          if (request.types
+                  .contains(WebViewPermissionResourceType.microphone) ||
+              request.types.contains(WebViewPermissionResourceType.camera)) {
+            await request.grant();
+          } else {
+            await request.deny();
+          }
+        });
+    }
+
+    if (ctrl.platform is WebKitWebViewController) {
+      final ios = ctrl.platform as WebKitWebViewController;
+      ios.setOnPlatformPermissionRequest((request) async => request.grant());
+    }
+
+    setState(() => _controller = ctrl); // ② lo asignamos de golpe
   }
 
   @override
@@ -86,21 +57,17 @@ class _AgentScreenState extends State<AgentScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          // Agregamos Padding con mu00e1rgenes superior e inferior segu00fan el dispositivo
           Padding(
             padding: EdgeInsets.only(
               top: MediaQuery.of(context).padding.top,
-              // Aumentamos el margen inferior sumando un valor adicional al padding del sistema
               bottom: MediaQuery.of(context).padding.bottom + 80,
             ),
-            child: WebViewWidget(controller: controller),
+            child: _controller == null // ③
+                ? const SizedBox() // mientras se crea
+                : WebViewWidget(controller: _controller!), // cuando ya existe
           ),
-          if (isLoading)
-            const Center(
-              child: CircularProgressIndicator(
-                color: Colors.cyan,
-              ),
-            ),
+          //if (isLoading)
+          //const Center(child: CircularProgressIndicator(color: Colors.cyan)),
         ],
       ),
     );
