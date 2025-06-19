64a3eaab39d861fd288e1bded801ca71434926cf
Marco Grijalva <marco@neubox.net>
2025-06-18 20:22:15 -0600

    fvm run proyect specific version 3.24.3

--- lib/src/config/app_config.dart
+++ lib/src/config/app_config.dart
@@ -2,6 +2,6 @@ class AppConfig {
   static const bool isProduction = bool.fromEnvironment('dart.vm.product');
 
   static String get apiBaseUrl {
-    return 'https://cwy1x0ovo9p0.share.zrok.io/';
+    return 'https://api.gebesa-app.com/';
   }
 }
